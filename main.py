1245e02f3197a698b1e55caf7c8db67b93781a28
axcelcamp ricky <axcelcamp.teacher@gmail.com>
2024-04-30 17:12:19 +0900

    ファイル名変更

--- main.py
+++ main.py
@@ -14,15 +14,18 @@ import win32gui
 import time
 
 from pygame import gfxdraw
+
+
 def send_to_clipboard(clip_type, data):
     win32clipboard.OpenClipboard()
     win32clipboard.EmptyClipboard()
     win32clipboard.SetClipboardData(clip_type, data)
     win32clipboard.CloseClipboard()
 
+
 def grab(x, y, w, h):
     im = ImageGrab.grab(bbox=(x, y, w, h))
-    im.save('im.png')
+    im.save("im.png")
     image = Image.open("im.png")
     output = BytesIO()
     image.convert("RGB").save(output, "BMP")
@@ -30,29 +33,43 @@ def grab(x, y, w, h):
     output.close()
     send_to_clipboard(win32clipboard.CF_DIB, data)
 
+
 pygame.init()
 info = pygame.display.Info()
 w = info.current_w
 h = info.current_h
-screen = pygame.display.set_mode((w, h), pygame.NOFRAME) # For borderless, use pygame.NOFRAME
+screen = pygame.display.set_mode((w, h), pygame.NOFRAME)  
 done = False
 fuchsia = (255, 0, 128)  # Transparency color
 dark_red = (255, 0, 0)
 
 # Create layered window
 hwnd = pygame.display.get_wm_info()["window"]
-win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE,
-                       win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
+win32gui.SetWindowLong(
+    hwnd,
+    win32con.GWL_EXSTYLE,
+    win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED,
+)
 # Set window transparency color
 # win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*fuchsia), 0, win32con.LWA_COLORKEY)
-win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*fuchsia), 50, win32con.LWA_ALPHA)
+win32gui.SetLayeredWindowAttributes(
+    hwnd, win32api.RGB(*fuchsia), 50, win32con.LWA_ALPHA
+)
 font = pygame.font.SysFont("Arial", 72)
 text = []
-text.append((font.render("Click on top left of the part you want to grab", 0, dark_red), (0, 10)))
+text.append(
+    (
+        font.render("Click on top left of the part you want to grab", 0, dark_red),
+        (0, 10),
+    )
+)
 text.append((font.render("Press Esc to close the window", 0, dark_red), (0, 100)))
+
+
 def show_text():
     for t in text:
         screen.blit(t[0], t[1])
+
 
 click = 0
 click1 = 0
@@ -72,14 +89,16 @@ while not done:
             if click1 == 0:
                 x1, y1 = pygame.mouse.get_pos()
                 print(x1, y1)
-                
+
                 click1 = 1
             elif click1 == 1:
                 x2, y2 = pygame.mouse.get_pos()
                 dx = x1 + (x2 - x1)
-                dy = int(y1 + ((x2-x1)/9*16))
-                
-                win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*fuchsia), 0, win32con.LWA_ALPHA)
+                dy = int(y1 + ((x2 - x1) / 9 * 16))
+
+                win32gui.SetLayeredWindowAttributes(
+                    hwnd, win32api.RGB(*fuchsia), 0, win32con.LWA_ALPHA
+                )
                 grab(x1, y1, dx, dy)
                 click1 = 0
                 done = True
@@ -93,7 +112,7 @@ while not done:
     elif click1 == 1:
         mx2, my2 = pygame.mouse.get_pos()
         x2 = mx2 - x1
-        y2 = x2/9*16
+        y2 = x2 / 9 * 16
 
     pygame.draw.rect(screen, (0, 255, 255), pygame.Rect(mx, my, x2, y2))
     pygame.display.update()
